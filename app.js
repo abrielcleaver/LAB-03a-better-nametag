3ca35177a3d5a0f9ec436e867f421be308d6df90
Abriel Cleaver <abrielcleaver@gmail.com>
2021-09-29 15:24:28 -0700

    add JS for previous nametag & update HTML/CSS

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 // import functions and grab DOM elements
 
+const firstName = document.getElementById('first-name');
+const button = document.getElementById('generate-entry');
+const newName = document.getElementById('new-name');
+const textBoxP = document.getElementById('text-box-p')
+
+
 // initialize global state
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+button.addEventListener('click', ()=> {
+  // post-click logic goes here
+  //when the button is clicked
+  // get the value of the input
+  // replace the text in the nametag with the value
+  
+  let nameValue = newName.value;
+  firstName.textContent = nameValue;
+
+  let pronounValue = textBoxP.value;
+  pronouns.textContent = pronounValue;
+});
+
+      
